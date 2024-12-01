f49d39cc586d055396f1c27edc198e1dfa240078
Jimmeh <jimmeh.mcdonald@gmail.com>
2024-12-01 12:58:49 +0000

    Make cmd accept input file arg

--- go/cmd/day1/day1.go
+++ go/cmd/day1/day1.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/Jimmeh/AOC2024/go/internal/file_reading"
 )
 
 func main() {
-	reader := file_reading.NewReader("./input")
+	filepath := os.Args[1]
+	reader := file_reading.NewReader(filepath)
 	lines, err := reader.Lines()
 	if err != nil {
 		log.Fatalf("Error reading file: %s", err)
