d818cfa1676dd1fc59ee287d1d447769159e5989
Jimmeh <jimmeh.mcdonald@gmail.com>
2024-12-03 18:59:22 +0000

    Test for 2 instructions

--- go/internal/day3/day3_test.go
+++ go/internal/day3/day3_test.go
@@ -11,11 +11,12 @@ func TestInstructions(t *testing.T) {
 	Case(t, "Single instruction with 1x1=1", "mul(1,1)", 1)
 	Case(t, "No instruction", "", 0)
 	Case(t, "No instruction but a lot of chars", "sadfjhasdjfhakjbsdaihdf3829uohanvcval;dsfjw9u3ru09gtsv", 0)
-	Case(t, "Single isntruction with 1x2=2", "mul(1,2)", 2)
+	Case(t, "Single instruction with 1x2=2", "mul(1,2)", 2)
 	Case(t, "Nearly an instruction (negative number)", "mul(-1,2)", 0)
 	Case(t, "Nearly an instruction (spaces)", "mul(1 ,2)", 0)
 	Case(t, "Instruction in the middle of stuff", "afhijiakfd adaslfmul(1,2)asdfijoas", 2)
 	Case(t, "Two instructions 1x2 + 1x2 = 4", "mul(1,2)mul(1,2)", 4)
+	Case(t, "Single instruction with 10x10=100", "mul(10,10)", 100)
 }
 
 func Case(t *testing.T, description string, input string, expected int) {
