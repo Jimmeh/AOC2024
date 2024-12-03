f921f5f33817948ed85ca66abcab1aab6cbafef1
Jimmeh <jimmeh.mcdonald@gmail.com>
2024-12-03 18:57:02 +0000

    test for instruction in the middle of stuff

--- go/internal/day3/day3_test.go
+++ go/internal/day3/day3_test.go
@@ -7,12 +7,14 @@ import (
 	"github.com/Jimmeh/AOC2024/go/test/helpers"
 )
 
-func TestSingleInstruction(t *testing.T) {
+func TestInstructions(t *testing.T) {
 	Case(t, "Single instruction with 1x1=1", "mul(1,1)", 1)
 	Case(t, "No instruction", "", 0)
 	Case(t, "No instruction but a lot of chars", "sadfjhasdjfhakjbsdaihdf3829uohanvcval;dsfjw9u3ru09gtsv", 0)
 	Case(t, "Single isntruction with 1x2=2", "mul(1,2)", 2)
-	Case(t, "Nearly an instruction", "mul(-1,2)", 0)
+	Case(t, "Nearly an instruction (negative number)", "mul(-1,2)", 0)
+	Case(t, "Nearly an instruction (spaces)", "mul(1 ,2)", 0)
+	Case(t, "Instruction in the middle of stuff", "afhijiakfd adaslfmul(1,2)asdfijoas", 2)
 }
 
 func Case(t *testing.T, description string, input string, expected int) {
