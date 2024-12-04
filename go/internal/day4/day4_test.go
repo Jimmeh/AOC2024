b862e8ce0466357d80e458e9e4a1542c50edeaed
Jimmeh <jimmeh.mcdonald@gmail.com>
2024-12-04 18:08:53 +0000

    Add test for exact match twice.

--- go/internal/day4/day4_test.go
+++ go/internal/day4/day4_test.go
@@ -10,6 +10,7 @@ import (
 func TestHorizontalMatches(t *testing.T) {
 	XmasMatchesCase(t, "Horizontal: No matches", []string{"ABCDE"}, 0)
 	XmasMatchesCase(t, "Horizontal: Exact match", []string{"XMAS"}, 1)
+	XmasMatchesCase(t, "Horizontal: Exact match twice", []string{"XMASXMAS"}, 2)
 }
 
 func XmasMatchesCase(t *testing.T, descrption string, input []string, expected int) {
